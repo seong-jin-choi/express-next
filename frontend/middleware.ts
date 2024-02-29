12301a14ff77e5784fd89dbac34875b9d3330c49
CODESPACE THOR <144303807+seong-jin-choi@users.noreply.github.com>
2024-02-29 14:07:38 +0900

    프로젝트 교체

--- frontend/middleware.ts
+++
@@ -1,7 +0,0 @@
-import next from "next";
-import { NextResponse } from "next/server";
-import type { NextRequest } from "next/server";
-
-export function middleware(request: NextRequest) {
-  return NextResponse.next();
-}
---
+++ frontend/src/pages/test/index.tsx
@@ -0,0 +1,237 @@
+// ** React Imports
+import { useState, MouseEvent, useCallback } from 'react'
+
+// ** MUI Imports
+import Box from '@mui/material/Box'
+import Card from '@mui/material/Card'
+import Grid from '@mui/material/Grid'
+import Divider from '@mui/material/Divider'
+import MenuItem from '@mui/material/MenuItem'
+import Typography from '@mui/material/Typography'
+import CardHeader from '@mui/material/CardHeader'
+import InputLabel from '@mui/material/InputLabel'
+import FormControl from '@mui/material/FormControl'
+import CardContent from '@mui/material/CardContent'
+import { DataGrid, GridColDef } from '@mui/x-data-grid'
+import Select, { SelectChangeEvent } from '@mui/material/Select'
+
+// ** Custom Components Imports
+import CustomChip from 'src/@core/components/mui/chip'
+
+// ** Types Imports
+import { ThemeColor } from 'src/@core/layouts/types'
+
+// ** Custom Table Components Imports
+import TableHeader from './components/TableHeader'
+import { fakeUsers } from './fakeUser'
+
+interface UserStatusType {
+  [key: string]: ThemeColor
+}
+
+interface CellType {
+  row: UsersType
+}
+
+const userStatusObj: UserStatusType = {
+  승인: 'success',
+  '승인 대기': 'warning',
+  미승인: 'secondary'
+}
+
+type UsersType = {
+  id: number
+  role: string
+  email: string
+  status: string
+  fullName: string
+  username: string
+}
+
+const columns: GridColDef[] = [
+  {
+    flex: 0.1,
+    minWidth: 110,
+    field: 'id',
+    headerName: '번호',
+    renderCell: ({ row }: CellType) => {
+      return (
+        <Box sx={{ display: 'flex', alignItems: 'center' }}>
+          <Typography noWrap sx={{ color: 'text.secondary', textTransform: 'capitalize' }}>
+            {row.id}
+          </Typography>
+        </Box>
+      )
+    }
+  },
+  {
+    flex: 0.25,
+    minWidth: 280,
+    field: 'fullName',
+    headerName: '이름',
+    renderCell: ({ row }: CellType) => {
+      return (
+        <Box sx={{ display: 'flex', alignItems: 'center' }}>
+          <Box sx={{ display: 'flex', alignItems: 'flex-start', flexDirection: 'column' }}>
+            <Typography
+              noWrap
+              sx={{
+                fontWeight: 500,
+                color: 'text.secondary'
+              }}
+            >
+              {row.fullName}
+            </Typography>
+          </Box>
+        </Box>
+      )
+    }
+  },
+  {
+    flex: 0.15,
+    field: 'role',
+    minWidth: 170,
+    headerName: '역할',
+    renderCell: ({ row }: CellType) => {
+      return (
+        <Box sx={{ display: 'flex', alignItems: 'center' }}>
+          <Typography noWrap sx={{ color: 'text.secondary', textTransform: 'capitalize' }}>
+            {row.role}
+          </Typography>
+        </Box>
+      )
+    }
+  },
+  {
+    flex: 0.1,
+    minWidth: 110,
+    field: 'status',
+    headerName: '승인 상태',
+    renderCell: ({ row }: CellType) => {
+      return (
+        <CustomChip
+          rounded
+          skin='light'
+          size='small'
+          label={row.status}
+          color={userStatusObj[row.status]}
+          sx={{ textTransform: 'capitalize' }}
+        />
+      )
+    }
+  }
+]
+
+const UserList = () => {
+  // ** State
+  const [testUser, setTestUser] = useState(fakeUsers)
+  const [role, setRole] = useState<string>('')
+  const [plan, setPlan] = useState<string>('')
+  const [value, setValue] = useState<string>('')
+  const [status, setStatus] = useState<string>('')
+  const [paginationModel, setPaginationModel] = useState({ page: 0, pageSize: 10 })
+
+  const handleFilter = useCallback((val: string) => {
+    setValue(val)
+  }, [])
+
+  const handleRoleChange = useCallback((e: SelectChangeEvent) => {
+    setRole(e.target.value)
+  }, [])
+
+  const handlePlanChange = useCallback((e: SelectChangeEvent) => {
+    setPlan(e.target.value)
+  }, [])
+
+  const handleStatusChange = useCallback((e: SelectChangeEvent) => {
+    setStatus(e.target.value)
+  }, [])
+
+  return (
+    <Grid container spacing={6.5}>
+      <Grid item xs={12}>
+        <Card>
+          <CardHeader title='검색 필터' />
+          <CardContent>
+            <Grid container spacing={6}>
+              <Grid item sm={4} xs={12}>
+                <FormControl fullWidth>
+                  <InputLabel id='role-select'>Select Role</InputLabel>
+                  <Select
+                    fullWidth
+                    value={role}
+                    id='select-role'
+                    label='Select Role'
+                    labelId='role-select'
+                    onChange={handleRoleChange}
+                    inputProps={{ placeholder: 'Select Role' }}
+                  >
+                    <MenuItem value=''>Select Role</MenuItem>
+                    <MenuItem value='admin'>Admin</MenuItem>
+                    <MenuItem value='author'>Author</MenuItem>
+                    <MenuItem value='editor'>Editor</MenuItem>
+                    <MenuItem value='maintainer'>Maintainer</MenuItem>
+                    <MenuItem value='subscriber'>Subscriber</MenuItem>
+                  </Select>
+                </FormControl>
+              </Grid>
+              <Grid item sm={4} xs={12}>
+                <FormControl fullWidth>
+                  <InputLabel id='plan-select'>Select Plan</InputLabel>
+                  <Select
+                    fullWidth
+                    value={plan}
+                    id='select-plan'
+                    label='Select Plan'
+                    labelId='plan-select'
+                    onChange={handlePlanChange}
+                    inputProps={{ placeholder: 'Select Plan' }}
+                  >
+                    <MenuItem value=''>Select Plan</MenuItem>
+                    <MenuItem value='basic'>Basic</MenuItem>
+                    <MenuItem value='company'>Company</MenuItem>
+                    <MenuItem value='enterprise'>Enterprise</MenuItem>
+                    <MenuItem value='team'>Team</MenuItem>
+                  </Select>
+                </FormControl>
+              </Grid>
+              <Grid item sm={4} xs={12}>
+                <FormControl fullWidth>
+                  <InputLabel id='status-select'>Select Status</InputLabel>
+                  <Select
+                    fullWidth
+                    value={status}
+                    id='select-status'
+                    label='Select Status'
+                    labelId='status-select'
+                    onChange={handleStatusChange}
+                    inputProps={{ placeholder: 'Select Role' }}
+                  >
+                    <MenuItem value=''>Select Role</MenuItem>
+                    <MenuItem value='pending'>Pending</MenuItem>
+                    <MenuItem value='active'>Active</MenuItem>
+                    <MenuItem value='inactive'>Inactive</MenuItem>
+                  </Select>
+                </FormControl>
+              </Grid>
+            </Grid>
+          </CardContent>
+          <Divider sx={{ m: '0 !important' }} />
+          <TableHeader value={value} handleFilter={handleFilter} />
+          <DataGrid
+            autoHeight
+            rowHeight={62}
+            rows={testUser}
+            columns={columns}
+            disableRowSelectionOnClick
+            pageSizeOptions={[10, 25, 50]}
+            paginationModel={paginationModel}
+            onPaginationModelChange={setPaginationModel}
+          />
+        </Card>
+      </Grid>
+    </Grid>
+  )
+}
+
+export default UserList
