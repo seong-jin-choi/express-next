1a1859145940aed77fbe4e109ee4296152f70ba0
CODESPACE THOR <144303807+seong-jin-choi@users.noreply.github.com>
2024-03-05 10:16:48 +0900

    폰트

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,9 +1,10 @@
 import StyledComponentsRegistry from '@/src/lib/registry'
 import '../../styles/globals.css'
+import { pretendard } from '../fonts'
 
 export default async function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang='en'>
+    <html lang='en' className={pretendard.className}>
       <body>
         <StyledComponentsRegistry>{children}</StyledComponentsRegistry>
       </body>
